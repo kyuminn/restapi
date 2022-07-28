eb6e347770611bb6b93e3b92307b420740d32987
kyuminn <92900475+kyuminn@users.noreply.github.com>
2022-07-28 16:08:29 +0900

    Delete untitled directory

--- untitled/src/main/java/com/mnsoft/upmu/common/util/ws/SitemapWS.java
+++
@@ -1,9 +0,0 @@
-package com.mnsoft.upmu.common.util.ws;
-
-public interface SitemapWS extends javax.xml.rpc.Service {
-    public String getSitemapWSSoapAddress();
-
-    public SitemapWSSoap getSitemapWSSoap() throws javax.xml.rpc.ServiceException;
-
-    public SitemapWSSoap getSitemapWSSoap(java.net.URL portAddress) throws javax.xml.rpc.ServiceException;
-}
--- untitled/src/main/java/com/mnsoft/upmu/common/util/ws/SitemapWSSoap.java
+++
@@ -1,9 +0,0 @@
-package com.mnsoft.upmu.common.util.ws;
-
-public interface SitemapWSSoap extends java.rmi.Remote {
-
-    /**
-     * 암호화된 문장에 대한 복호화
-     */
-    public String getPlainText(String strEncID, String strCompanyCode, String strEncText) throws java.rmi.RemoteException;
-}
